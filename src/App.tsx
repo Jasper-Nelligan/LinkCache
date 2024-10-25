9790c36f3b7d5509a82f5603a3575691615a58eb
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-10-25 08:06:57 -0700

    Add linkData argument to LinkGroups for dynamic rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import "../index.css"
 import Header from "./Header"
 import LinkGroups from "./LinkGroups"
+import { LinkInfo } from "./types"
+
+const tempLinkData: LinkInfo[][] = [
+  [
+    {
+      name: "Facebook",
+      url: "https://facebook.com",
+      icon: "/facebook_logo.png"
+    },
+    {
+      name: "Twitter",
+      url: "https://twitter.com",
+      icon: "/x_logo.png"
+    },
+    {
+      name: "Instagram",
+      url: "https://instagram.com",
+      icon: "/instagram_logo.png"
+    },
+    {
+      name: "Reddit",
+      url: "https://reddit.com",
+      icon: "/reddit_logo.png"
+    },
+  ]
+]
 
 function App() {
   return (
     <>
       <Header />
-      <LinkGroups />
+      <LinkGroups linkData={tempLinkData} />
     </>
   )
 }
--- src/LinkGroups.tsx
+++ src/LinkGroups.tsx
@@ -1,33 +1,16 @@
 import LinkGroup from "./components/LinkGroup";
 import { LinkInfo } from "./types";
 
-export default function LinkGroups() {
-  const tempLinks: LinkInfo[] = [
-    {
-      name: "Facebook",
-      url: "https://facebook.com",
-      icon: "/facebook_logo.png"
-    },
-    {
-      name: "Twitter",
-      url: "https://twitter.com",
-      icon: "/x_logo.png"
-    },
-    {
-      name: "Instagram",
-      url: "https://instagram.com",
-      icon: "/instagram_logo.png"
-    },
-    {
-      name: "Reddit",
-      url: "https://reddit.com",
-      icon: "/reddit_logo.png"
-    },
-  ]
+export default function LinkGroups({linkData}: {linkData: LinkInfo[][]}) {
+  const renderLinkGroups = linkData.map((links, index) => {
+    return (
+      <LinkGroup links={links} key={index} />
+    )
+  });
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4">
-      <LinkGroup links={tempLinks}/>
+      {renderLinkGroups}
     </div>
   )
 }
