6222241e3cd6e3ac67413e73b0889882661f086a
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-10-28 20:27:24 -0700

    Fix initial link group showing wrong color

--- src/components/colorOptions.tsx
+++ src/components/colorOptions.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { colors } from "@/constants";
 import { useEffect, useState } from "react";
 
 export default function ColorOptions(
@@ -13,7 +14,6 @@ export default function ColorOptions(
   }, [selectedColor, onColorChange]);
 
   const renderColorOptions = () => {
-    const colors = ["#FE7B7B", "#75D073", "#74A3FF", "#FFE977", "#FBB143", "#C17ED9"];
     return colors.map((color, index) => (
       <Button className={`${selectedColor === color ? "border-2 border-bg-gray-300" : ""}`} type="button" variant="ghost" size="icon" onClick={() => setSelectedColor(color)} key={index}>
         <div style={{ backgroundColor: color }} className="rounded-full h-6 w-6" />
--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,35 @@
 import { LinkGroupInfo } from "./types";
 
+export const colors = ["#FE7B7B", "#75D073", "#74A3FF", "#FFE977", "#FBB143", "#C17ED9"];
+
+export const emptyLinkGroupInfo: LinkGroupInfo = {
+  id: -1,
+  linkGroupName: "",
+  color: colors[0],
+  linkPairs: [
+    {
+      name: "",
+      url: "",
+    },
+    {
+      name: "",
+      url: "",
+    },
+    {
+      name: "",
+      url: "",
+    },
+    {
+      name: "",
+      url: "",
+    },
+  ],
+}
+
 export const initialLinkGroupInfo: LinkGroupInfo = {
   id: 0,
   linkGroupName: "Social Media",
-  color: "#ff5733",
+  color: colors[0],
   linkPairs: [
     {
       name: "Facebook",
@@ -20,30 +46,6 @@ export const initialLinkGroupInfo: LinkGroupInfo = {
     {
       name: "Reddit",
       url: "https://reddit.com",
-    },
-  ],
-}
-
-export const emptyLinkGroupInfo: LinkGroupInfo = {
-  id: -1,
-  linkGroupName: "",
-  color: "#ff5733",
-  linkPairs: [
-    {
-      name: "",
-      url: "",
-    },
-    {
-      name: "",
-      url: "",
-    },
-    {
-      name: "",
-      url: "",
-    },
-    {
-      name: "",
-      url: "",
     },
   ],
 }
