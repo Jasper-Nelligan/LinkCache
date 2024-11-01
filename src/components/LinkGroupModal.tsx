460d1c0d1b56fa90945e7dc5ee938e1d12376ceb
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-11-01 10:13:20 -0700

    Change add link btn color to green

--- src/components/LinkGroupModal.tsx
+++ src/components/LinkGroupModal.tsx
@@ -167,7 +167,7 @@ export default function LinkGroupModal(
               <div className="mt-4 mb-4">
                 {renderLinkPairs()}
               </div>
-              <Button className="mb-4" type="button" onClick={() => append({ name: "", url: "" })}>
+              <Button className="mb-4 bg-green-500" type="button" onClick={() => append({ name: "", url: "" })}>
                 Add Link
               </Button>
               <DialogFooter className="flex">
