7efd6fff87a37b640da4e6475d6bf4d4a5ae9a11
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-12-09 11:17:51 -0800

    Add Github icon to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,6 +25,13 @@ export default function Header() {
               <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
               <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             </Button>
+            <Button onClick={() => window.open('https://github.com/Jasper-Nelligan/LinkCache', '_blank')} variant="ghost" size="icon">
+              <img
+              src={theme === "light" ? "/github-mark.png" : "/github-mark-white.png"}
+              alt="GitHub"
+              className="h-[1.2rem] w-[1.2rem]"
+              />
+            </Button>
           </div>
       </div>
     </header>
