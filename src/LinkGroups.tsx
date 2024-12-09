4a5e91b4c5dc5a1983b5b2972e543b9fe30c0d53
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-12-09 11:37:06 -0800

    Change border to white when dark mode is turned on

--- src/LinkGroups.tsx
+++ src/LinkGroups.tsx
@@ -13,7 +13,7 @@ function LinkGroups({ linkGroupInfoArray, onOpenModal }: { linkGroupInfoArray: L
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4">
       {renderLinkGroups}
-      <div className="flex justify-center items-center rounded-md border-2 border-dashed p-4 cursor-pointer">
+      <div className="flex justify-center items-center rounded-md border-2 border-dashed p-4 cursor-pointer dark:border-white">
         <Button asChild onClick={() => onOpenModal(-1)}>
             <Plus size={48} data-testid="new-link-group-btn" color="#ffffff" />
         </Button>
